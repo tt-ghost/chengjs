94535b3bf1a540c68d9009bb6551019bd1847938
Chengzi <ttghost@126.com>
2022-10-23 23:24:45 +0800

    feat: update tracking

--- packages/tracking/src/Designer.ts
+++ packages/tracking/src/Designer.ts
@@ -17,10 +17,12 @@ export default class Designer {
       console.log(e.target)
       e.preventDefault()
       e.stopPropagation()
+
       const hasTarget = !!this.trackings.filter(({ el }) => el === e.target)
         .length
+
       if (!hasTarget) this.trackings.push({ el: e.target })
-      this.refreshPane()
+      this.render()
     }
     this.init()
   }
@@ -39,9 +41,13 @@ export default class Designer {
       '*{outline: 1px solid rgba(0,0,0,.1)}' +
       '*:hover{outline: 1px solid rgba(0,0,0,.3)}' +
       '*:focus{outline: 1px solid rgba(0,0,200,.3)}' +
-      '#cj-tracking-pane{position: fixed;right:0;bottom:0;width: 200px;min-height: 300px;' +
+      '#cj-tracking-pane{' +
+      'outline-none;' +
+      'position: fixed;right:0;bottom:0;width: 200px;min-height: 300px;' +
       'background-color: rgba(255,255,255,.6);padding: 16px;}' +
+      '#cj-tracking-pane *{outline: none;}'
       '#cj-tracking-pane .cj-tracking-pane-item{margin: 8px 0;background-color: rgba(255,255,255,.8)}'
+
     el.innerText = style
     document.head.appendChild(el)
   }
@@ -52,20 +58,24 @@ export default class Designer {
 
   createPane() {
     const pane = document.createElement('div')
+
     pane.setAttribute('id', 'cj-tracking-pane')
     document.body.append(pane)
+
     return pane
   }
 
-  refreshPane() {
+  render() {
     this.pane.innerHTML = this.trackings
       .map(({ el }) => {
         let text = '#' + (el.getAttribute('id') || '')
         text = text === '#' ? Array.from(el.classList).join('.') : '匿名'
+
         return `<div class='cj-tracking-pane-item'>${text}</div>`
       })
       .join('')
   }
+
   destroy() {
     document.body.removeChild(document.getElementById(this.styleId))
     document.body.removeEventListener('click', this.bodyListener, true)
