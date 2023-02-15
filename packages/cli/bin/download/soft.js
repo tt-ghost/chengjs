a1b142728712c86b40d17698f3df3deedb894be8
Chengzi <ttghost@126.com>
2023-02-15 22:50:25 +0800

    feat: add soft

---
+++ packages/cli/bin/download/soft.js
@@ -0,0 +1,59 @@
+const clients = {
+  WeChat: {
+    title: '微信',
+    name: 'WeChat',
+    url: {
+      mac: 'https://dldir1.qq.com/weixin/mac/WeChatMac.dmg',
+      win: 'https://dldir1.qq.com/weixin/Windows/WeChatSetup.exe'
+    }
+  },
+  NeteaseMusic: {
+    title: '网易云音乐',
+    name: 'NeteaseMusic',
+    url: {
+      mac: 'https://d1.music.126.net/dmusic/NeteaseMusic_2.3.12_956_web.dmg',
+      win: 'https://d1.music.126.net/dmusic/NeteaseCloudMusic_Music_official_2.10.6.200601.exe'
+    }
+  },
+  Chrome: {
+    title: '网易云音乐',
+    name: 'NeteaseMusic',
+    url: {
+      mac: 'https://dl.google.com/chrome/mac/universal/stable/GGRO/googlechrome.dmg',
+      win: 'https://d1.music.126.net/dmusic/NeteaseCloudMusic_Music_official_2.10.6.200601.exe'
+    }
+  },
+  QQ: {
+    title: 'QQ',
+    name: 'QQ',
+    url: {
+      mac: 'https://dldir1.qq.com/qqfile/qq/QQNT/e9f0c326/QQ_v6.9.6-8827.dmg',
+      win: 'https://dldir1.qq.com/qqfile/qq/PCQQ9.7.3/QQ9.7.3.28946.exe'
+    }
+  },
+  SublimeText: {
+    title: 'SublimeText',
+    name: 'SublimeText',
+    url: {
+      mac: 'https://download.sublimetext.com/sublime_text_build_4143_mac.zip',
+      win: 'https://download.sublimetext.com/sublime_text_build_4143_x64_setup.exe'
+    }
+  },
+  TencentVideo: {
+    title: '腾讯视频',
+    name: 'TencentVideo',
+    url: {
+      mac: 'https://dldir1.qq.com/qqtv/mac/TencentVideo2.63.0.53547.dmg',
+      win: 'https://dldir1.qq.com/qqtv/TencentVideo11.63.2019.0.exe'
+    }
+  },
+  iTerm: {
+    title: 'iTerm',
+    name: 'iTerm',
+    url: {
+      mac: 'https://iterm2.com/downloads/stable/iTerm2-3_4_19.zip',
+      win: 'https://iterm2.com/downloads/stable/iTerm2-3_4_19.zip'
+    }
+  },
+
+}
--- packages/cli/bin/main.js
+++ packages/cli/bin/main.js
@@ -1,23 +1,26 @@
 #!/usr/bin/env node
-const { Command } = require('commander')
-const inquirer = require('inquirer')
-const pkg = require('../package.json')
+// const { Command } = require('commander')
+import { Command } from 'commander'
+import inquirer from 'inquirer'
+// const inquirer = require('inquirer')
+// import pkg from '../package.json'
+// const pkg = require('../package.json')
 
 const program = new Command()
-const prompts = [
-  {
-    name: 'desc',
-    message: '请输入描述信息',
-    default: pkg.description || ''
-  },
-  {
-    name: 'author',
-    message: '请输入作者',
-    default: pkg.author || ''
-  }
-]
+// const prompts = [
+//   {
+//     name: 'desc',
+//     message: '请输入描述信息',
+//     default: pkg.description || ''
+//   },
+//   {
+//     name: 'author',
+//     message: '请输入作者',
+//     default: pkg.author || ''
+//   }
+// ]
 
-program.name(pkg.name).description(pkg.description).version(pkg.version)
+// program.name(pkg.name).description(pkg.description).version(pkg.version)
 
 program.description('获取版本').option('-v, --version', '获取版本')
 
@@ -33,18 +36,18 @@ program.description('获取版本').option('-v, --version', '获取版本')
 //     console.log('answers: ', answers)
 //   });
 
-program
-  .command('build')
-  .description('打包')
-  .argument('<string>', 'string to split')
-  .option('--first', 'display just the first substring')
-  .option('-s, --separator <char>', 'separator character', ',')
-  .action(async (str, options) => {
-    const limit = options.first ? 1 : undefined
-    console.log(str.split(options.separator, limit))
-    const answers = await inquirer.prompt(prompts)
-    console.log('answers: ', answers)
-  })
+// program
+//   .command('build')
+//   .description('打包')
+//   .argument('<string>', 'string to split')
+//   .option('--first', 'display just the first substring')
+//   .option('-s, --separator <char>', 'separator character', ',')
+//   .action(async (str, options) => {
+//     const limit = options.first ? 1 : undefined
+//     console.log(str.split(options.separator, limit))
+//     const answers = await inquirer.prompt(prompts)
+//     console.log('answers: ', answers)
+//   })
 
 program
   .command('init')
@@ -53,7 +56,7 @@ program
   // .option('--first', 'display just the first substring')
   // .option('-s, --separator <char>', 'separator character', ',')
   .action(async (str, options) => {
-    console.log(999, str, options)
+    // console.log(999, str, options)
     // 1. download template
     // 2. add standard
     // 3. check project
